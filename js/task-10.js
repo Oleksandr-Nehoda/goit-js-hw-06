1fb560cb6c8449133e31855c263d35ac6fb43c52
Oleksandr-Nehoda <iamsoul@ukr.net>
2022-08-24 17:13:11 +0300

    task-10

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,41 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const inputRef = document.querySelector('[ type="number"]');
+const btnCreateRef = document.querySelector('[ data-create]');
+const btnDestroyRef = document.querySelector('[ data-destroy]');
+const boxesRef = document.querySelector('#boxes');
+
+console.log(btnCreateRef);
+
+
+btnCreateRef.addEventListener('click', onAddDivClick );
+
+function onAddDivClick()  {
+  const inputValue = inputRef.value;
+  createBoxes(inputValue);
+} 
+
+
+function createBoxes(amount) {
+  for (let i = 0; i < amount ; i += 1 ) {
+
+    const newDivRef = document.createElement('div');
+    let sizeDiv = 30 + i*10;
+
+    newDivRef.style.width = `${sizeDiv}px`;
+    newDivRef.style.height = `${sizeDiv}px`;
+    newDivRef.style.backgroundColor = getRandomHexColor();
+
+    boxesRef.appendChild(newDivRef);
+  }
+}
+
+btnDestroyRef.addEventListener('click', destroyBoxes );
+
+function destroyBoxes() {
+
+  boxesRef.innerHTML = " ";
+
+}
