9e003b2c2d26c0ef886040fcdf843fcd1c018a07
Oleksandr-Nehoda <iamsoul@ukr.net>
2022-08-24 15:17:56 +0300

    task - 09

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,20 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const btnRef = document.querySelector('.change-color');
+const spanRef = document.querySelector('.color');
+const bodyRef = document.querySelector('body');
+
+
+btnRef.addEventListener('click', onChangeColorClick);
+
+
+function onChangeColorClick() {
+
+  const bodyColor = bodyRef.style.backgroundColor=getRandomHexColor() ;
+  
+  console.log( bodyColor);
+
+  spanRef.textContent = bodyColor;
+}
